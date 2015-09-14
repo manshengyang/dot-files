d464d403806122ead9031b7eaabb292d8172abcd
myang <myang@dytechlab.com>
2015-09-14 09:51:01 +0800

    fix link script

--- scripts/link.sh
+++ scripts/link.sh
@@ -4,6 +4,6 @@ cur=`pwd`
 for f in $(find "$cur" -name ".??*"); do
   name=$(basename $f)
   if [[ "$name" != .git ]]; then
-    [[ ! -e ~/$name || -L ~/$name ]] && ln -sf "$name" $HOME
+    [[ ! -e ~/$name || -L ~/$name ]] && ln -sf "$f" $HOME
   fi
 done
