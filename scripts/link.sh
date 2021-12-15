0fd7918f24a0455038a4f6be2ab0b89374b0609a
Mansheng Yang <mansheng.yang@gmail.com>
2021-12-15 22:11:32 +0800

    update

--- scripts/link.sh
+++ scripts/link.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+cd $(dirname $(dirname $(readlink -f $0)))
 cur=`pwd`
+echo $cur
+if [[ $(basename $cur) != "dot-files" ]]; then
+  echo "Invalid dir"
+  exit 1
+fi
 for f in $(find "$cur" -name ".??*"); do
   name=$(basename $f)
   if [[ "$name" != .git ]]; then
+    echo "link $name"
     [[ ! -e ~/$name || -L ~/$name ]] && ln -sf "$f" $HOME
   fi
 done
